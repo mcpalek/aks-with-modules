e6b7e839a877880699c1dc37808bd349a175aa95
mcpalek <73556463+mcpalek@users.noreply.github.com>
2024-12-24 16:31:53 +0100

    Update backend.tf

--- aks/backend.tf
+++ aks/backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-aks-test-001"
+    storage_account_name = "testaksstorage13"
+    container_name       = "tfstatecontainer"
+    key                  = "devops.terraform.tfstate"
+  }
+}
 
